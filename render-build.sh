fc40fe1e141ea8a454273489fe7976effde64617
DiogoDavi <diogopb_25@hotmail.com>
2026-05-16 10:40:29 -0300

    fix: pachage

--- render-build.sh
+++ render-build.sh
@@ -2,10 +2,17 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+# Instala as dependências do projeto
 npm install
 
-# Install Chrome for Puppeteer
+# Define o diretório de cache local para o Puppeteer
+export PUPPETEER_CACHE_DIR=$HOME/.cache/puppeteer
+
+# Limpa o cache existente para evitar o erro de "folder exists but executable missing"
+echo "Cleaning Puppeteer cache..."
+rm -rf $PUPPETEER_CACHE_DIR
+
+# Instala o Chrome para o Puppeteer
 echo "Installing Chrome for Puppeteer..."
 npx puppeteer browsers install chrome
 
